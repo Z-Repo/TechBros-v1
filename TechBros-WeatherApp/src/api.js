22af7dfb785871d4b5887c6714ae187f67ca1abc
Rocco <roccotrovato126@gmail.com>
2023-07-02 22:50:58 -0400

    Added Geo API and fetch call

---
+++ TechBros-WeatherApp/src/api.js
@@ -0,0 +1,10 @@
+export const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c5657e40e6msh2b21adccd4a33fap1b4602jsnbcc74fccdc2f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
--- TechBros-WeatherApp/src/components/search.jsx
+++ TechBros-WeatherApp/src/components/search.jsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { options, GEO_API_URL } from "../api";
 
 const Search = (onSearchChange) => {
+  const [search, setSearch] = useState(null);
 
-    const [search, setSearch] = useState(null);
+  const loadOptions = (inputValue) => {
+    return fetch(GEO_API_URL, options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
 
-    const handleOnChange = (searchData) => {
-        setSearch(searchData);
-        onSearchChange(searchData);
-    }
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
 
-    return (
-        <>
-        <AsyncPaginate
+  return (
+      <AsyncPaginate
         placeholder="Search for city"
         debounceTimeout={600}
         value={search}
         onChange={handleOnChange}
-        />
-        </>
-    )
-}
+        loadOptions={loadOptions}
+      />
+  );
+};
 
 export default Search;
