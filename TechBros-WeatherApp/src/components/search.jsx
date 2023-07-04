3d33f257b7f87c839b9d6289e2450b2a81e51953
Rocco <roccotrovato126@gmail.com>
2023-07-04 04:52:23 -0400

    Search suggestions display city, countryCode and return lat/long to console

--- TechBros-WeatherApp/src/components/search.jsx
+++ TechBros-WeatherApp/src/components/search.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { options, GEO_API_URL } from "../api";
+import { geoApiOptions, geoApiUrl } from "./APIs/Geo/geoApi";
 
-const Search = (onSearchChange) => {
+const Search = ({onSearchChange}) => {
   const [search, setSearch] = useState(null);
 
   const loadOptions = (inputValue) => {
-    return fetch(GEO_API_URL, options)
+    return fetch(`https://${geoApiUrl}?namePrefix=${inputValue}`, geoApiOptions)
       .then((response) => response.json())
-      .then((response) => console.log(response))
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
       .catch((err) => console.error(err));
   };
 
@@ -18,13 +27,13 @@ const Search = (onSearchChange) => {
   };
 
   return (
-      <AsyncPaginate
-        placeholder="Search for city"
-        debounceTimeout={600}
-        value={search}
-        onChange={handleOnChange}
-        loadOptions={loadOptions}
-      />
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
   );
 };
 
