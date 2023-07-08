cccfaad936e277be7683e5bece2975ced33d4e00
Rocco <roccotrovato126@gmail.com>
2023-07-08 15:31:01 -0400

    Current weather HTML/CSS template updated

--- src/components/current-weather/currentWeather.jsx
+++ src/components/current-weather/currentWeather.jsx
@@ -1,13 +1,41 @@
-import './currentWeather.css';
+import "./currentWeather.css";
 
 const CurrentWeather = () => {
-    return <div className='weather'>
-            <div className='top'>
-                <p className='city'>Philadelphia</p>
-                <p className='weather-description'>Sunny</p>
-            </div>
-            <img alt='weather' className='weather-icon' src='img/Clear.png'/>
-        </div>;
-}
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">Philadelphia</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="img/Clear.png" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels Like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15 hPa</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CurrentWeather;
