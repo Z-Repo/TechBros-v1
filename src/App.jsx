2885bbc732560b78cdbc4d8c116dd30171180808
Z <z@CodeBreaker-3.local>
2023-07-09 18:40:38 -0500

    I've got a working version of google maps API running, we just have to reverse engeer it to learn how it works, then customize it to be suitable for our needs. You'll need my .env for the API key with google.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import "./App.css";
 import "./Layout/mainBackGround.css";
 import Search from "./components/SearchBar/search";
 import CurrentWeather from "./components/current-weather/currentWeather";
+import Map from "./components/Map/Map";
+import { useState } from "react";
 
 function App() {
+  const [position, setPosition] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
     console.log(searchData);
+    setPosition(searchData.value);
   };
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       <CurrentWeather />
+      <Map position={[37.77493, -122.41944]} zoom={10} mapType="roadMap" />
     </div>
   );
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,120 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+// Importing some Hooks and components from google maps api which we will be using
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
 
-const Map = () => {
-  return <div>Map</div>;
+const libraries = ["places"];
+const Map = (position, zoom, mapType) => {
+  // We will be using some state variables to store the response from the google maps api
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const sourceRef = useRef();
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destinationRef = useRef();
+
+  // We Have to select some coordinates to give to the map to initially show the map from that point.
+  // This are the latitude and longitude of New Delhi, India
+  const center = { lat: 28.7041, lng: 77.1025 };
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: import.meta.env.VITE_GMA_KEY,
+    libraries: libraries,
+  });
+
+  // function to calculate Route
+  async function calculateRoute() {
+    event.preventDefault();
+    if (sourceRef.current.value === "" || destinationRef.current.value === "") {
+      return;
+    }
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: sourceRef.current.value,
+      destination: destinationRef.current.value,
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
+  }
+
+  function clearRoute() {
+    setDirectionsResponse(null);
+    setDistance("");
+    setDuration("");
+    sourceRef.current.value = "";
+    destinationRef.current.value = "";
+  }
+
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return (
+    <>
+      <main className="container">
+        {/*<div className="sub-container">
+           <form className="container-form" onSubmit={calculateRoute}>
+            <Autocomplete>
+              <input type="text" placeholder="Enter Source" ref={sourceRef} />
+            </Autocomplete>
+
+            <br />
+            <Autocomplete>
+              <input
+                type="text"
+                placeholder="Enter Destination"
+                ref={destinationRef}
+              />
+            </Autocomplete>
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+
+          <button className="self-center" onClick={clearRoute}>
+            Clear Route
+          </button>
+
+          {directionsResponse && (
+            <div className="self-center">
+              <div>Distance = {distance}</div>
+              <div>Duration= {duration}</div>
+            </div>
+          )}
+        </div> */}
+
+        <div style={{ width: "50vw", height: "100vh" }}>
+          <GoogleMap
+            center={center}
+            zoom={15}
+            mapContainerStyle={{
+              width: "100%",
+              height: "100%",
+              borderRadius: "2rem",
+            }}
+            options={{
+              streetViewControl: true,
+              zoomControl: false,
+              mapTypeControl: false,
+              fullscreenControl: false,
+            }}
+          >
+            {console.log(directionsResponse)}
+            <Marker position={center} />
+            {directionsResponse && (
+              <DirectionsRenderer directions={directionsResponse} />
+            )}
+          </GoogleMap>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Map;
